1a41e2543a4cc9e4b0b12b39c18b4090d248ed7e
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-11-01 17:47:43 -0400

    finished part 2

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const app = express();
 
 // Songs ROUTES
 const songsController = require("./controllers/songsController.js");
-app.use("/songs", songsController);
 
-// 404 PAGE
-app.get("*", (req, res) => {
-  res.status(404).send("Page not found.");
-});
+// // 404 PAGE
+// app.get("*", (req, res) => {
+//   res.status(404).send("Page not found.");
+// });
 
 //Import cors
 const cors = require("cors");
 
 // Middleware
 app.use(cors());
+// Middleware that allows the server to accept json
 app.use(express.json());
+// Middleware...When the URL starts with /songs, use the songsController to route us appropriately
+app.use("/songs", songsController);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Tuner Full Stack Application!");
--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,12 +1,42 @@
 // controllers/songsController.js
 const express = require("express");
-const { getAllSongs } = require("../queries/song.js");
 const songs = express.Router();
+const { getAllSongs, getSong, createSong } = require("../queries/song");
+const {
+  checkName,
+  checkArtist,
+  checkBoolean
+} = require("../validations/checkSongs");
 
-// Get all songs
+// GET ROUTE to get all songs
+// localhost:3350/songs/
 songs.get("/", async (req, res) => {
   const allSongs = await getAllSongs();
-  res.json(allSongs);
+  if (allSongs[0]) {
+    res.status(200).json(allSongs);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+// GET one song
+// localhost:3350/songs/:id
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const oneSong = await getSong(id);
+  if (oneSong) {
+    res.status(200).json(oneSong);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
+
+// Post a new song
+// localhost:3350/songs/
+songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
+  const body = req.body;
+  const song = await createSong(body);
+  res.status(200).json(song);
 });
 
 module.exports = songs;
